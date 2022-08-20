1f69d1b0f1b90d52471059368f9c0ced7eaed723
Sergey Rutkovsky <dr.bibelman@gmail.com>
2022-08-20 08:58:54 +0300

    Анимирует лоадер

--- project/src/pages/loading-screen/loading-screen.tsx
+++ project/src/pages/loading-screen/loading-screen.tsx
@@ -1,6 +1,20 @@
+import './loading-screeen.css';
 function LoadingScreen(): JSX.Element {
   return (
-    <p>Loading ...</p>
+
+
+    <div className="spinner-wrapper">
+      <div className="spinner">
+        <div className="sk-folding-cube">
+          <div className="sk-cube1 sk-cube"></div>
+          <div className="sk-cube2 sk-cube"></div>
+          <div className="sk-cube4 sk-cube"></div>
+          <div className="sk-cube3 sk-cube"></div>
+        </div>
+      </div>
+    </div>
+
+
   );
 }
 
